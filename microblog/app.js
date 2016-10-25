7bee3d1a3061e8c68a7bc0c99eae05914416536a
Vito Lee <plummoon@gmail.com>
2016-10-25 20:24:32 +0800

    1:使用ejs模板引擎

--- microblog/app.js
+++ microblog/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var demos = require('./routes/demos');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/demos', demos);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
